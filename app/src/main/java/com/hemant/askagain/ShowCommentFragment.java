314c638d04b77971e936956832dac698bf069fcd
123archie <93096219+123archie@users.noreply.github.com>
2022-09-10 17:10:16 +0530

    comments added

--- app/src/main/java/com/hemant/askagain/ShowCommentFragment.java
+++ app/src/main/java/com/hemant/askagain/ShowCommentFragment.java
@@ -1,5 +1,4 @@
 package com.hemant.askagain;
-
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatButton;
@@ -7,6 +6,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,13 +16,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 public class ShowCommentFragment extends Fragment {
-
     private RecyclerView recyclerView;
     private AppCompatButton commentBtn;
     private String postID;
     private CommentModel commentData;
     static ArrayList<CommentModel> commentList = new ArrayList<>();
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -31,7 +29,6 @@ public class ShowCommentFragment extends Fragment {
         initViews(view);
         getBundle();
         getSetAllComment();
-
         commentBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,8 +49,8 @@ public class ShowCommentFragment extends Fragment {
     private void getBundle() {
         Bundle bundle = this.getArguments();
         postID = bundle.getString("PostId");
-
-    }
+        Log.d("TAG","postID: "+postID);
+        }
 
     private void getSetAllComment() {
         CommentAdapter commentAdapter = new CommentAdapter(commentList,getContext());
