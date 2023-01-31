c595423f6a2c4498d3a74caaaab95f1e2d0b0539
123archie <93096219+123archie@users.noreply.github.com>
2023-01-31 10:43:56 +0530

    Error Rectified

--- app/src/main/java/com/hemant/askagain/DashboardFragment.java
+++ app/src/main/java/com/hemant/askagain/DashboardFragment.java
@@ -1,12 +1,14 @@
 package com.hemant.askagain;
+
 import static android.view.View.GONE;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -19,10 +21,10 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.annotations.Nullable;
 
 import java.util.ArrayList;
 public class DashboardFragment extends Fragment {
-
     static ArrayList<PostModel> postList = new ArrayList<>();
     private RecyclerView recyclerView;
     private FloatingActionButton fabAddPostBtn;
@@ -34,10 +36,10 @@ public class DashboardFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
         initViews(view);
-//        @Override
+
 //        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 //            super.onViewCreated(view, savedInstanceState);
-            getAllPost();
+//            getAllPost();
 //        }
         if(fabAddPostBtn.getVisibility()==GONE){
             fabAddPostBtn.setVisibility(View.VISIBLE);
@@ -56,7 +58,7 @@ public class DashboardFragment extends Fragment {
 
     private void openAddPostFragment(Fragment fragment) {
         FragmentTransaction fragmentTransaction= requireActivity().getSupportFragmentManager().beginTransaction();
-//        GoogleSignInAccount acct= GoogleSignIn.getLastSignedInAccount(getContext());
+        GoogleSignInAccount acct= GoogleSignIn.getLastSignedInAccount(getContext());
         Bundle bundle = new Bundle();
         Log.d("TAG", "Bundle: "+bundle);
         bundle.putString("Name", name );
@@ -66,10 +68,10 @@ public class DashboardFragment extends Fragment {
         fragment.setArguments(bundle);
         fragmentTransaction.replace(R.id.fragment,fragment).addToBackStack(null).commit();
     }
-
-
-
-
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getAllPost();
+    }
     private void getAllPost() {
         PostAdapter postAdapter = new PostAdapter(postList, getContext());
         Log.d("TAG", "postAdapter: "+postAdapter);
